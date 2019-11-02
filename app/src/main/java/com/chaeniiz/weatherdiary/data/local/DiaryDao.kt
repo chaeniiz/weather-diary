46953e961c533681d2dab14523ed91dd8a04a84e
송채윤 <indexor18@gmail.com>
2019-11-02 16:38:44 +0900

    Add DAO

---
+++ app/src/main/java/com/chaeniiz/weatherdiary/data/local/DiaryDao.kt
@@ -0,0 +1,21 @@
+package com.chaeniiz.weatherdiary.data.local
+
+import androidx.room.*
+
+@Dao
+interface DiaryDao {
+    @Query("SELECT * FROM diary")
+    fun getDiaries(): List<Diary>
+
+    @Query("SELECT * FROM diary where id = :id")
+    fun getDiary(id: Int): Diary
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertDiary(diary: Diary)
+
+    @Update
+    fun updateDiary(diary: Diary)
+
+    @Delete
+    fun deleteDiary(diary: Diary)
+}
