7ac2a1fded092a1a5bcb0351372019164f1f4e80
송채윤 <indexor18@gmail.com>
2019-11-08 00:32:27 +0900

    Add converters to convert `Date`

--- app/src/main/java/com/chaeniiz/weatherdiary/data/local/database/DiaryDatabase.kt
+++ app/src/main/java/com/chaeniiz/weatherdiary/data/local/database/DiaryDatabase.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.chaeniiz.weatherdiary.data.local.Converters
 import com.chaeniiz.weatherdiary.data.local.DiaryDao
 import com.chaeniiz.weatherdiary.data.local.model.Diary
 
 @Database(entities = [Diary::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class DiaryDatabase: RoomDatabase() {
     abstract fun diaryDao(): DiaryDao
 
